7457fa08c146256ab402ef2e49e1d8e89d8d829e
Radek <rgosciminski@simplito.com>
2025-05-05 16:47:21 +0200

    clean up exports in counter_server

--- counter/src/counter_server.erl
+++ counter/src/counter_server.erl
@@ -2,10 +2,9 @@
 
 -behaviour(gen_server).
 
--export([start_link/0, increment/1, decrement/1, get/1]).
+-export([start_link/0, increment/1, decrement/1, get/1, list_counters/0]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3,
          reset/2]).
--export([list_counters/0]).
 
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
