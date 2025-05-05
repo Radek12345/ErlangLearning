e30c5a4c6876b9318cfe0c5defbf133deeb3199d
Radek <rgosciminski@simplito.com>
2025-05-05 14:19:51 +0200

    feat: multiple counters

--- counter/src/counter_api.erl
+++ counter/src/counter_api.erl
@@ -1,65 +1,113 @@
 -module(counter_api).
 
--export([start_link/0, init/2]).
+-export([init/2, start_link/0]).
 
 start_link() ->
     Dispatch =
         cowboy_router:compile([{'_',
-                                [{"/counter", ?MODULE, []},
-                                 {"/counter/increment", ?MODULE, []},
-                                 {"/counter/decrement", ?MODULE, []},
-                                 {"/counter/reset", ?MODULE, []}]}]),
+                                [{"/counters", ?MODULE, []},
+                                 {"/counters/:id", ?MODULE, []},
+                                 {"/counters/:id/increment", ?MODULE, []},
+                                 {"/counters/:id/decrement", ?MODULE, []},
+                                 {"/counters/:id/reset", ?MODULE, []}]}]),
     {ok, _} =
         cowboy:start_clear(counter_api, [{port, 8080}], #{env => #{dispatch => Dispatch}}),
     {ok, self()}.
 
-init(Req = #{method := <<"GET">>, path := <<"/counter">>}, State) ->
-    Value = counter_server:get(),
-    Req2 =
-        cowboy_req:reply(200,
-                         #{<<"content-type">> => <<"application/json">>},
-                         jiffy:encode(#{value => Value}),
-                         Req),
-    {ok, Req2, State};
-init(Req = #{method := <<"POST">>, path := <<"/counter/increment">>}, State) ->
-    counter_server:increment(),
-    Req2 =
-        cowboy_req:reply(200,
-                         #{<<"content-type">> => <<"application/json">>},
-                         jiffy:encode(#{status => <<"ok">>}),
-                         Req),
-    {ok, Req2, State};
-init(Req = #{method := <<"POST">>, path := <<"/counter/decrement">>}, State) ->
-    counter_server:decrement(),
-    Req2 =
-        cowboy_req:reply(200,
-                         #{<<"content-type">> => <<"application/json">>},
-                         jiffy:encode(#{status => <<"ok">>}),
-                         Req),
-    {ok, Req2, State};
-init(Req = #{method := <<"POST">>, path := <<"/counter/reset">>}, State) ->
-    {ok, Body, Req1} = cowboy_req:read_body(Req),
-    case jiffy:decode(Body, [return_maps]) of
-        #{<<"value">> := NewValue} when is_integer(NewValue) ->
-            counter_server:reset(NewValue),
-            Req2 =
-                cowboy_req:reply(200,
-                                 #{<<"content-type">> => <<"application/json">>},
-                                 jiffy:encode(#{status => <<"ok">>}),
-                                 Req1),
-            {ok, Req2, State};
+init(Req0, State) ->
+    Method = cowboy_req:method(Req0),
+    Path = cowboy_req:path(Req0),
+    error_logger:info_msg("Method: ~p, Path: ~p~n", [Method, Path]),
+    case {Method, Path} of
+        {<<"GET">>, <<"/counters">>} ->
+            Counters = counter_server:list_counters(),
+            error_logger:info_msg("GET /counters, counters: ~p~n", [Counters]),
+            Req = cowboy_req:reply(200,
+                                   #{<<"content-type">> => <<"application/json">>},
+                                   jiffy:encode(#{counters =>
+                                                      [atom_to_binary(C, utf8) || C <- Counters]}),
+                                   Req0),
+            {ok, Req, State};
+        {<<"GET">>, _} ->
+            IdBin = cowboy_req:binding(id, Req0),
+            case is_valid_id(IdBin) of
+                true ->
+                    CounterId = binary_to_atom(IdBin, utf8),
+                    Value = counter_server:get(CounterId),
+                    error_logger:info_msg("GET /counters/~p, value: ~p~n", [CounterId, Value]),
+                    Req = cowboy_req:reply(200,
+                                           #{<<"content-type">> => <<"application/json">>},
+                                           jiffy:encode(#{value => Value}),
+                                           Req0),
+                    {ok, Req, State};
+                false ->
+                    error_logger:info_msg("Invalid counter ID: ~p~n", [IdBin]),
+                    reply_error(400, <<"Invalid counter ID">>, Req0, State)
+            end;
+        {<<"POST">>, Path} ->
+            IdBin = cowboy_req:binding(id, Req0),
+            case is_valid_id(IdBin) of
+                true ->
+                    CounterId = binary_to_atom(IdBin, utf8),
+                    ExpectedIncrementPath = <<"/counters/", IdBin/binary, "/increment">>,
+                    ExpectedDecrementPath = <<"/counters/", IdBin/binary, "/decrement">>,
+                    ExpectedResetPath = <<"/counters/", IdBin/binary, "/reset">>,
+                    case Path of
+                        ExpectedIncrementPath ->
+                            error_logger:info_msg("POST /counters/~p/increment~n", [CounterId]),
+                            counter_server:increment(CounterId),
+                            reply_ok(Req0, State);
+                        ExpectedDecrementPath ->
+                            error_logger:info_msg("POST /counters/~p/decrement~n", [CounterId]),
+                            counter_server:decrement(CounterId),
+                            reply_ok(Req0, State);
+                        ExpectedResetPath ->
+                            error_logger:info_msg("POST /counters/~p/reset received~n",
+                                                  [CounterId]),
+                            {ok, Body, Req1} = cowboy_req:read_body(Req0),
+                            error_logger:info_msg("Body: ~p~n", [Body]),
+                            try jiffy:decode(Body, [return_maps]) of
+                                #{<<"value">> := NewValue} when is_integer(NewValue) ->
+                                    error_logger:info_msg("Calling counter_server:reset(~p, ~p)~n",
+                                                          [CounterId, NewValue]),
+                                    counter_server:reset(CounterId, NewValue),
+                                    reply_ok(Req1, State);
+                                _ ->
+                                    error_logger:info_msg("Invalid JSON format~n"),
+                                    reply_error(400, <<"Invalid or missing value">>, Req1, State)
+                            catch
+                                error:badarg ->
+                                    error_logger:info_msg("JSON decode failed: badarg~n"),
+                                    reply_error(400, <<"Invalid JSON">>, Req1, State)
+                            end;
+                        _ ->
+                            error_logger:info_msg("Unknown POST path: ~p~n", [Path]),
+                            reply_error(404, <<"Not found">>, Req0, State)
+                    end;
+                false ->
+                    error_logger:info_msg("Invalid counter ID: ~p~n", [IdBin]),
+                    reply_error(400, <<"Invalid counter ID">>, Req0, State)
+            end;
         _ ->
-            Req2 =
-                cowboy_req:reply(400,
-                                 #{<<"content-type">> => <<"application/json">>},
-                                 jiffy:encode(#{error => <<"Invalid or missing value">>}),
-                                 Req1),
-            {ok, Req2, State}
-    end;
-init(Req, State) ->
+            error_logger:info_msg("Unknown request: Method=~p, Path=~p~n", [Method, Path]),
+            reply_error(404, <<"Not found">>, Req0, State)
+    end.
+
+reply_ok(Req, State) ->
     Req2 =
-        cowboy_req:reply(404,
+        cowboy_req:reply(200,
                          #{<<"content-type">> => <<"application/json">>},
-                         jiffy:encode(#{error => <<"Not found">>}),
+                         jiffy:encode(#{status => <<"ok">>}),
                          Req),
     {ok, Req2, State}.
+
+reply_error(Code, Msg, Req, State) ->
+    Req2 =
+        cowboy_req:reply(Code,
+                         #{<<"content-type">> => <<"application/json">>},
+                         jiffy:encode(#{error => Msg}),
+                         Req),
+    {ok, Req2, State}.
+
+is_valid_id(Id) ->
+    is_binary(Id) andalso byte_size(Id) > 0 andalso byte_size(Id) =< 32.
--- counter/src/counter_server.erl
+++ counter/src/counter_server.erl
@@ -2,44 +2,52 @@
 
 -behaviour(gen_server).
 
--export([start_link/0, increment/0, decrement/0, get/0]).
+-export([start_link/0, increment/1, decrement/1, get/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3,
-         reset/1]).
+         reset/2]).
+-export([list_counters/0]).
 
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
-increment() ->
-    gen_server:cast(?MODULE, increment).
+increment(Id) ->
+    gen_server:cast(?MODULE, {increment, Id}).
 
-decrement() ->
-    gen_server:cast(?MODULE, decrement).
+decrement(Id) ->
+    gen_server:cast(?MODULE, {decrement, Id}).
 
-get() ->
-    gen_server:call(?MODULE, get).
+get(Id) ->
+    gen_server:call(?MODULE, {get, Id}).
 
-reset(NewValue) ->
-    gen_server:cast(?MODULE, {reset, NewValue}).
+reset(Id, NewValue) ->
+    gen_server:cast(?MODULE, {reset, Id, NewValue}).
+
+list_counters() ->
+    gen_server:call(?MODULE, list_counters).
 
 init([]) ->
-    {ok, load_counter()}.
+    {ok, #{}}.
 
-handle_call(get, _From, State) ->
-    {reply, State, State};
+handle_call({get, Id}, _From, State) ->
+    Value = load_counter(Id),
+    {reply, Value, State};
+handle_call(list_counters, _From, State) ->
+    Keys = mnesia:dirty_all_keys(counter),
+    {reply, Keys, State};
 handle_call(_Msg, _From, State) ->
     {reply, ok, State}.
 
-handle_cast(increment, State) ->
-    NewState = State + 1,
-    ok = save_counter(NewState),
-    {noreply, NewState};
-handle_cast(decrement, State) ->
-    NewState = State - 1,
-    ok = save_counter(NewState),
-    {noreply, NewState};
-handle_cast({reset, NewValue}, _State) ->
-    ok = save_counter(NewValue),
-    {noreply, NewValue};
+handle_cast({increment, Id}, State) ->
+    NewState = load_counter(Id) + 1,
+    ok = save_counter(Id, NewState),
+    {noreply, State};
+handle_cast({decrement, Id}, State) ->
+    NewState = load_counter(Id) - 1,
+    ok = save_counter(Id, NewState),
+    {noreply, State};
+handle_cast({reset, Id, NewValue}, State) ->
+    ok = save_counter(Id, NewValue),
+    {noreply, State};
 handle_cast(Msg, State) ->
     io:format("UNHANDLED cast: ~p~n", [Msg]),
     {noreply, State}.
@@ -53,14 +61,14 @@ terminate(_Reason, _State) ->
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
-load_counter() ->
-    case mnesia:dirty_read(counter, counter_id) of
-        [{counter, counter_id, Value}] ->
+load_counter(Id) ->
+    case mnesia:dirty_read(counter, Id) of
+        [{counter, Id, Value}] ->
             Value;
         [] ->
-            ok = mnesia:dirty_write({counter, counter_id, 0}),
+            ok = mnesia:dirty_write({counter, Id, 0}),
             0
     end.
 
-save_counter(Value) ->
-    mnesia:dirty_write({counter, counter_id, Value}).
+save_counter(Id, Value) ->
+    mnesia:dirty_write({counter, Id, Value}).
